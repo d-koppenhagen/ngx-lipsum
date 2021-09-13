0841841bd76308b6b0ea5ab5dcfd0ec98e4ccf99
Danny Koppenhagen <mail@d-koppenhagen.de>
2021-09-13 12:57:15 +0200

    test(ngx-lipsum-component): test component

--- projects/ngx-lipsum/src/lib/ngx-lipsum.component.spec.ts
+++ projects/ngx-lipsum/src/lib/ngx-lipsum.component.spec.ts
@@ -1,15 +1,22 @@
+import { SimpleChange } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { LipsumService } from './lipsum.service';
 import { NgxLipsumComponent } from './ngx-lipsum.component';
 
 describe('NgxLipsumComponent', () => {
   let component: NgxLipsumComponent;
   let fixture: ComponentFixture<NgxLipsumComponent>;
+  let mockLipsumService: LipsumService;
+  let mockLipsumServiceSpy: jest.SpyInstance;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [NgxLipsumComponent],
     }).compileComponents();
+    mockLipsumService = TestBed.inject(LipsumService);
+    mockLipsumServiceSpy = jest
+      .spyOn(mockLipsumService, 'get')
+      .mockReturnValue('mocked response');
   });
 
   beforeEach(() => {
@@ -20,5 +27,24 @@ describe('NgxLipsumComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('OnInit: should set the text value by asking the LipsumService', () => {
+    expect(mockLipsumServiceSpy).toHaveBeenCalledTimes(1);
+    expect(component.text).toEqual('mocked response');
+  });
+
+  it('OnChanges: should set the text value by asking the LipsumService with a new custom config', () => {
+    component.config = {
+      count: 100,
+    };
+    component.ngOnChanges({
+      config: {
+        currentValue: {
+          count: 100,
+        },
+      } as SimpleChange,
+    });
+    expect(mockLipsumServiceSpy).toHaveBeenCalledWith({ count: 100 });
   });
 });
